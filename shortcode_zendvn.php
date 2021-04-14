17162d4150e643f7085f00c8621a5ae0f74a0001
Nguyễn Hữu Tiến <nguyenhuutien.it.3895@gmail.com>
2021-04-14 07:24:41 +0700

    Update shortcode_zendvn.php

--- shortcode_zendvn.php
+++ shortcode_zendvn.php
@@ -1,3 +1,13 @@
+// #1 Shortcode ZendVn
+//
+// add_shortcode('tien_my_sc_date', 'tien_mp_show_date');
+
+// function tien_mp_show_date()
+// {
+// 	$str = date('l jS \of F Y h:i:s A');
+
+// 	return $str;
+// }
 
 // Shortcode with my class
 class Tien_Sc_Date
@@ -9,13 +19,21 @@ class Tien_Sc_Date
 
 	public function show()
 	{
-		$str = date('l jS \of F Y h:i:s A');
+		echo '<br/>' . get_the_ID();
+		$postObj = get_post(get_the_ID());
+
+		if (has_shortcode($postObj->post_content, 'tien_my_sc_date') == 1) {
+			echo "<pre>";
+			print_r($postObj);
+			echo "</pre>";
+
+			$str = date('l jS \of F Y h:i:s A');
+		}
 
 		return $str;
 	}
 }
 
-<?php
 // #2
 // new Tien_Sc_Date();
 
@@ -38,14 +56,25 @@ class Zendvn_Mp_SC_Main
 		$this->_shortcode_option = get_option($this->_shortcode_name, $defaultOption);
 
 		// echo '<br/>' .  __METHOD__;
-		if ($this->_shortcode_option == true) {
-			$this->date();
-		}
+		// if ($this->_shortcode_option == true) {
+		$this->date();
+		// }
+
+		// echo '<br/>shortcode exists: ' . shortcode_exists('tien_my_sc_date');
+
+
+		// #3 Remove shortcode
+		// loại bỏ phương thức đưa vào trong hook
+		// remove_shortcode('tien_my_sc_date');
 	}
 
 	public function date()
 	{
-		new Tien_Sc_Date();
+		if ($this->_shortcode_option == true) {
+			new Tien_Sc_Date();
+		} else {
+			add_shortcode('tien_my_sc_date', '__return_false');
+		}
 	}
 }
 
